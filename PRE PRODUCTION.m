3fa69812713a126d4279f3a89d4c0cfaba15f191
Aakash Sonkusare <72997132+sudo-cloud@users.noreply.github.com>
2021-08-04 20:55:51 +0530

    Update PRE PRODUCTION.m

--- PRE PRODUCTION.m
+++ PRE PRODUCTION.m
@@ -1,28 +1,41 @@
                                        PRE PRODUCTION
                                            
-                     START                  --> Understanding Software Development LifeCycle
+                     START                  ⇨ Understanding Software Development LifeCycle
                      
-       Plannig and Requirement Gathering    --> JIRA or Trello or Servicenow
+       Plannig and Requirement Gathering    ⇨ JIRA or Trello or Servicenow
        
-                  Programming               --> Python
+                  Programming               ⇨ Python
                   
-                 Versionining               --> GitHub or BitBucket or GitLab or TFS
+                 Versionining               ⇨ GitHub or BitBucket or GitLab or TFS
                  
-                 Code Analysis              --> SonarQube or Fortify
+                 Code Analysis              ⇨ SonarQube or Fortify
                  
-                Build Management            --> PyBuilder
+                Build Management            ⇨ PyBuilder
                 
-             Repository Management          --> Git or Nexus or Artifactory or Archiva or Nuget
+             Repository Management          ⇨ Git or Nexus or Artifactory or Archiva or Nuget
              
-      Development to QA, STaging, Preprod   --> Chef or Puppet or Ansible
+      Development to QA, STaging, Preprod   ⇨ Chef or Puppet or Ansible
       
-                   Container                --> Docker
+                   Container                ⇨ Docker
                     
-               Acceptance Testing           --> Selenium, Cucumber or Xunit
+               Acceptance Testing           ⇨ Selenium, Cucumber or Xunit
               
-                  Code Coverage             --> Coverage
+                  Code Coverage             ⇨ Coverage
                 
-              Continuous Integration        --> Jenkins or Sprinkler or Bamboo or Teamcity
+              Continuous Integration        ⇨ Jenkins or Sprinkler or Bamboo or Teamcity
             
+                                        
+                                        PRODUCTION
+  Deployment to Production (Existing Server) ⇨ Chef Or Puppet Or Ansible Or UDeploy Or Octopus Deploy
+  
+    Deployment to Production (Container)    ⇨ Kuernetes
+      
+         Infrastructure Monitoring	        ⇨	Nagios or Zabbix or New Relic Infrastructure or Datadog
+          
+              Log Monitoring	              ⇨	Splunk or ELBK
+      
+          Performance Monitoring	          ⇨	New Relic or Appdynamics
+          
+                 Security	                  ⇨	Appscan or SonarQube or Fortify
          
              
