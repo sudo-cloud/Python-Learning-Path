53c3173dbf4f35dffd81e8b8e55d6350b466fc0b
Aakash Sonkusare <72997132+sudo-cloud@users.noreply.github.com>
2021-08-04 08:16:35 +0530

    Update PRE PRODUCTION.m

--- PRE PRODUCTION.m
+++ PRE PRODUCTION.m
@@ -1,18 +1,28 @@
                                        PRE PRODUCTION
                                            
-                     START               --> Understanding Software Development LifeCycle
+                     START                  --> Understanding Software Development LifeCycle
                      
-       Plannig and Requirement Gathering --> JIRA or Trello or Servicenow
+       Plannig and Requirement Gathering    --> JIRA or Trello or Servicenow
        
-                  Programming            --> Python
+                  Programming               --> Python
                   
-                 Versionining            --> GitHub or BitBucket or GitLab or TFS
+                 Versionining               --> GitHub or BitBucket or GitLab or TFS
                  
-                 Code Analysis           --> SonarQube or Fortify
+                 Code Analysis              --> SonarQube or Fortify
                  
-                Build Management         --> PyBuilder
+                Build Management            --> PyBuilder
                 
-             Repository Management       --> Git or Nexus or Artifactory or Archiva or Nuget
+             Repository Management          --> Git or Nexus or Artifactory or Archiva or Nuget
+             
+      Development to QA, STaging, Preprod   --> Chef or Puppet or Ansible
+      
+                   Container                --> Docker
+                    
+               Acceptance Testing           --> Selenium, Cucumber or Xunit
+              
+                  Code Coverage             --> Coverage
+                
+              Continuous Integration        --> Jenkins or Sprinkler or Bamboo or Teamcity
             
          
              
